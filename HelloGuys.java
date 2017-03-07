35eea6af6e9c5366efd1f5f6317a3296289c60c7
Nightlilac <291053339@qq.com>
2017-03-07 16:46:22 +0800

    b

--- HelloGuys.java
+++ HelloGuys.java
@@ -2,6 +2,6 @@ public class HelloGuys {
 
 // main	
 public static void main(String[] args) {		
-        System.out.println("hello every!");	
+        System.out.println("hello everyooo!”);	
     }
 }
